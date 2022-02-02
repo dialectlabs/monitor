8800cfadbf5c46e80a9048cd9becb209775b08ec
Alexey Tsymbal <alexey@gotiggy.com>
2022-02-03 02:20:15 +0300

    Add some docs

--- src/README.md
+++ src/README.md
@@ -1,31 +1,8 @@
 # Terminology
 
 1. Data source is an abstraction that represents a set of resource-specific parameters that need to be ingested,
-   optionally transformed and sent to user. Each data source provides access to multiple resources.
+   optionally transformed and sent to user. Each data source provides access to multiple parameters.
 2. Resource is an abstraction that represents any entity that is addressable on chain (e.g. user or PDA)
 3. Parameter is an abstraction that represents a resource-bound data structure or scalar value that is stored on chain
 
 Data source <-- 1...n --> Resource <-- n...n --> Parameter
-
-# Requirements
-
-## Data model
-
-1. Single data source
-
-## Data ingestion patterns to be supported
-
-1. Pull (selected for MVP)
-2. Push
-
-## Data models supported
-
-1. Numeric scalar (selected for MVP)
-2. Any object
-
-## Data source patterns
-
-1. Push
-
-
-
--- src/monitor-api.ts
+++ src/monitor-api.ts
@@ -7,45 +7,73 @@ import { Program } from '@project-serum/anchor';
 import { DialectEventSink } from './internal/dialect-event-sink';
 import { OnChainSubscriberRepository } from './internal/on-chain-subscriber.repository';
 
+/**
+ * Parameter id is a unique name of monitored entity
+ */
 export type ParameterId = string;
 
+/**
+ * Parameter may have some metadata
+ */
 export type Parameter = {
   id: ParameterId;
   description?: string;
 };
 
+/**
+ * Parameter-bound data (i.e. parameter measurement)
+ */
 export type ParameterData<T> = { parameterId: ParameterId; data: T };
 
-// data source
 export type DataSourceMetadata = {
   id: string;
   parameters: Parameter[];
 };
 
-// resource
+/**
+ * A user or a dApp, just an alias to
+ */
 export type ResourceId = PublicKey;
 
+/**
+ * Resource-bound parameter data (i.e. a parameter measurement bound to a specific user)
+ */
 export type ResourceParameterData<T> = {
   resourceId: ResourceId;
   parameterData: ParameterData<T>;
 };
 
+/**
+ * A batch of multiple parameter data
+ */
 export type DataPackage<T> = ResourceParameterData<T>[];
 
+/**
+ * An abstraction that represents a source of data, bound to specific type
+ */
 export interface DataSource<T> {
   connect(): Promise<DataSourceMetadata>;
 
   disconnect(): Promise<void>;
 }
 
+/**
+ * Pollable data source will be polled by framework to get next datapackage
+ */
 export interface PollableDataSource<T> extends DataSource<T> {
   extract(subscribers: ResourceId[]): Promise<DataPackage<T>>;
 }
 
+/**
+ * A set of transformations that are executed on-top of unbound data stream to detect events
+ */
 export type EventDetectionPipeline<T> = (
   parameterData: Observable<ParameterData<T>>,
 ) => Observable<Event>;
 
+/**
+ * A resource, that have created a dialect with monitor => subscriber of events
+ */
 export type Subscriber = {
   resourceId: ResourceId;
   dialectAccount: DialectAccount;
@@ -56,21 +84,39 @@ export type SubscriberRemovedEventHandler = (
   subscriberResourceId: ResourceId,
 ) => any;
 
+/**
+ * Repository containing all subscribers, also provides subscribe semantics to get updates
+ */
 export interface SubscriberRepository {
+  /**
+   * Return all subscribers of the monitor
+   */
   findAll(): Promise<Subscriber[]>;
 
+  /**
+   * Finds subscriber by resource id
+   */
   findByResourceId(resourceId: ResourceId): Promise<Subscriber | null>;
 
+  /**
+   * Can be used to set handlers to react if set of subscribers is changed
+   */
   subscribe(
     onSubscriberAdded: SubscriberAddedEventHandler,
     onSubscriberRemoved: SubscriberRemovedEventHandler,
   ): any;
 }
 
+/**
+ * An interface that abstracts the destination where events are sent/persisted
+ */
 export interface EventSink {
   push(event: Event, recipients: ResourceId[]): Promise<void>;
 }
 
+/**
+ * A parameter state that was detected by pipeline and need to be sent to subscribers
+ */
 export interface Event {
   timestamp: Date;
   type: 'warning' | 'info';
@@ -78,12 +124,19 @@ export interface Event {
   message: string;
 }
 
+/**
+ * A monitor is an entity that is responsible for execution of unbounded streaming ETL (Extract, Transform, Load)
+ * and connects DataSource, EventDetectionPipeline and EventSink
+ */
 export interface Monitor<T> {
   start(): Promise<void>;
 
   stop(): Promise<void>;
 }
 
+/**
+ * A set of factory methods to create monitors
+ */
 export class Monitors {
   static createUnicast<T>(
     dataSource: PollableDataSource<T>,
