bf7f94d6b9315837178f9619b0675f659d68b9ca
Alexey Tsymbal <alexey@gotiggy.com>
2022-02-04 00:44:37 +0300

    Fix wrong import

--- src/internal/dialect-extensions.ts
+++ src/internal/dialect-extensions.ts
@@ -1,6 +1,6 @@
-import { getDialectForMembers } from '@dialectlabs/web3/index';
 import { PublicKey } from '@solana/web3.js';
 import { Program } from '@project-serum/anchor';
+import { getDialectForMembers } from '@dialectlabs/web3';
 
 // TODO: move to protocol
 export function getDialectAccount(
