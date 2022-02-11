8569b018883c9c477db41fe57e6b5c2abe9dbc1d
Alexey Tsymbal <alexey@gotiggy.com>
2022-02-11 18:46:57 +0300

    WIP high level pipelines definition

--- src/transformation-pipelines.ts
+++ src/transformation-pipelines.ts
@@ -1,6 +1,66 @@
-import { Data, SubscriberState } from './data-model';
+import { Data, Event, SubscriberState } from './data-model';
 import { Operators, PipeLogLevel } from './transformation-pipeline-operators';
 import { TransformationPipeline } from './ports';
+import { Duration } from 'luxon';
+
+export type Window = FixedSizeWindow | FixedSizeSlidingWindow | FixedTimeWindow;
+
+export interface FixedSizeWindow {
+  type: 'fixes-size';
+  size: number;
+}
+
+export interface FixedSizeSlidingWindow {
+  type: 'fixes-size-sliding';
+  size: number;
+}
+
+export interface FixedTimeWindow {
+  type: 'fixed-time';
+  timeSpan: Duration;
+}
+
+export type Trigger = RisingEdgeTrigger | FallingEdgeTrigger;
+
+export interface RisingEdgeTrigger {
+  type: 'rising-edge';
+}
+
+export interface FallingEdgeTrigger {
+  type: 'falling-edge';
+}
+
+export type RateLimit = ThrottleTimeRateLimit;
+
+export interface ThrottleTimeRateLimit {
+  type: 'throttle-time';
+  timeSpan: Duration;
+}
+
+export interface ThresholdProps {
+  trigger: Trigger;
+  window?: Window;
+  rateLimit?: RateLimit;
+}
+
+export interface EventGenerationProps<V> {
+  builder: (data: Data<V>) => Event;
+}
+
+export function threshold(
+  { window, trigger, rateLimit }: ThresholdProps,
+  eventGenerationProps: EventGenerationProps<number>,
+) {
+  const pipeline: TransformationPipeline<number> = (source) =>
+    source.pipe().pipe(
+      Operators.Event.info(
+        'Dummy numeric TransformationPipeline 1',
+        (v: Data<number>) =>
+          `Hello world for user ${v.resourceId} from p1 ${v}`,
+      ),
+      Operators.Utility.log(PipeLogLevel.INFO),
+    );
+}
 
 export const dummyNumericPipeline1: TransformationPipeline<number> = (source) =>
   source.pipe(
