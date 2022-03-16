95a102628805d1a74990b6dd55c31d1e3f77ac7c
Alexey Tsymbal <alexey@gotiggy.com>
2022-03-16 19:17:00 +0400

    Fix pushy data source example

--- examples/007-pushy-data-source-monitor.ts
+++ examples/007-pushy-data-source-monitor.ts
@@ -1,4 +1,10 @@
-import { Monitor, Monitors, Pipelines, SourceData } from '../src';
+import {
+  DialectNotification,
+  Monitor,
+  Monitors,
+  Pipelines,
+  SourceData,
+} from '../src';
 import { DummySubscriberRepository } from './003-custom-subscriber-repository';
 import { ConsoleNotificationSink } from './004-custom-notification-sink';
 import { Observable } from 'rxjs';
@@ -11,51 +17,46 @@ type DataType = {
 
 const threshold = 0.5;
 
+const consoleNotificationSink =
+  new ConsoleNotificationSink<DialectNotification>();
+
 const monitor: Monitor<DataType> = Monitors.builder({
   subscriberRepository: new DummySubscriberRepository(1),
-  notificationSink: new ConsoleNotificationSink(),
 })
   .defineDataSource<DataType>()
   .push(
     new Observable((subscriber) => {
       const publicKey = Keypair.generate().publicKey;
       const d1: SourceData<DataType> = {
-        data: { cratio: 1, healthRatio: 2 },
+        data: { cratio: 0, healthRatio: 2 },
         resourceId: publicKey,
       };
       const d2: SourceData<DataType> = {
-        data: { cratio: 0, healthRatio: 0 },
+        data: { cratio: 1, healthRatio: 0 },
         resourceId: publicKey,
       };
       subscriber.next(d1);
       subscriber.next(d2);
     }),
   )
-  .transform<number>({
+  .addTransformations<number, number>()
+  .transform({
     keys: ['cratio'],
     pipelines: [
-      Pipelines.threshold(
-        {
-          type: 'falling-edge',
-          threshold,
-        },
-        {
-          messageBuilder: ({ value, context: { origin } }) =>
-            `Your cratio = ${value} below warning threshold`,
-        },
-      ),
-      Pipelines.threshold(
-        {
-          type: 'rising-edge',
-          threshold,
-        },
-        {
-          messageBuilder: ({ value, context }) =>
-            `Your cratio = ${value} above warning threshold`,
-        },
-      ),
+      Pipelines.threshold({
+        type: 'rising-edge',
+        threshold,
+      }),
     ],
   })
+  .notify()
+  .custom<DialectNotification>(
+    ({ value }) => ({
+      message: `Your cratio = ${value} above warning threshold`,
+    }),
+    consoleNotificationSink,
+  )
+  .and()
   .dispatch('unicast')
   .build();
 monitor.start();
