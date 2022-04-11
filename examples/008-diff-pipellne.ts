5a83fa00851388ad9ac8987c3cd57824ebab6ba8
Alexey Tsymbal <alexey@gotiggy.com>
2022-04-12 03:24:15 +0400

    v1.2.4

---
+++ examples/008-diff-pipellne.ts
@@ -0,0 +1,56 @@
+import { DialectNotification, Monitor, Monitors, Pipelines, SourceData } from '../src';
+import { DummySubscriberRepository } from './003-custom-subscriber-repository';
+import { ConsoleNotificationSink } from './004-custom-notification-sink';
+import { Observable } from 'rxjs';
+import { Keypair } from '@solana/web3.js';
+
+type DataType = {
+  cratio: number;
+  healthRatio: number;
+};
+
+const threshold = 0.5;
+
+const consoleNotificationSink =
+  new ConsoleNotificationSink<DialectNotification>();
+
+const observable = new Observable((subscriber) => {
+  const publicKey = Keypair.generate().publicKey;
+  const d1: SourceData<DataType> = {
+    data: { cratio: 0, healthRatio: 2 },
+    resourceId: publicKey,
+  };
+  const d2: SourceData<DataType> = {
+    data: { cratio: 1, healthRatio: 0 },
+    resourceId: publicKey,
+  };
+  subscriber.next(d1);
+  subscriber.next(d2);
+});
+const monitor: Monitor<DataType> = Monitors.builder({
+  subscriberRepository: new DummySubscriberRepository(1),
+})
+  .defineDataSource<DataType>()
+  .push(
+    observable,
+  )
+  .transform<number, number>({
+    keys: ['cratio', 'healthRatio'],
+    pipelines: [
+      Pipelines.threshold({
+        type: 'rising-edge',
+        threshold,
+      }),
+    ],
+  })
+  .notify()
+  .custom<DialectNotification>(
+    ({ value }) => ({
+      message: `Your cratio = ${value} above warning threshold`,
+    }),
+    consoleNotificationSink,
+  )
+  .and()
+  .dispatch('unicast')
+  .build();
+monitor.start();
