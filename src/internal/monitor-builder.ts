27217f5cf2b5e96e5181d8f1e9bb7c6d7a964b0b
Alexey Tsymbal <tsymbal.aleksey@gmail.com>
2022-04-25 16:29:32 +0400

    Impove push model support (#22)
    * Filter available subscribers in dialect notification sink
    
    * Make transformations optional
    
    * Version 1.2.12
    
    * CR fixes
    
    Co-authored-by: Alexey Tsymbal <alexey@gotiggy.com>

--- src/internal/monitor-builder.ts
+++ src/internal/monitor-builder.ts
@@ -28,6 +28,8 @@ import {
   EmailNotification,
   SengridEmailNotificationSink,
 } from '../sengrid-email-notification-sink';
+import { OnChainSubscriberRepository } from './on-chain-subscriber.repository';
+import { InMemorySubscriberRepository } from './in-memory-subscriber.repository';
 
 /**
  * A set of factory methods to create monitors
@@ -42,9 +44,18 @@ export class MonitorsBuilderState<T extends object> {
 
   constructor(readonly monitorProps: MonitorProps) {
     if (monitorProps.dialectProgram && monitorProps.monitorKeypair) {
+      if (!monitorProps.subscriberRepository) {
+        const onChainSubscriberRepository = new OnChainSubscriberRepository(
+          monitorProps.dialectProgram,
+          monitorProps.monitorKeypair,
+        );
+        monitorProps.subscriberRepository =
+          InMemorySubscriberRepository.decorate(onChainSubscriberRepository);
+      }
       this.dialectNotificationSink = new DialectNotificationSink(
         monitorProps.dialectProgram,
         monitorProps.monitorKeypair,
+        monitorProps.subscriberRepository,
       );
     }
     const sinks = monitorProps?.sinks;
@@ -127,6 +138,30 @@ class AddTransformationsStepImpl<T extends object>
 
   constructor(private readonly monitorBuilderState: MonitorsBuilderState<T>) {
     monitorBuilderState.addTransformationsStep = this;
+  }
+
+  notify(): AddSinksStep<T, T> {
+    const identityTransformation: DataSourceTransformationPipeline<
+      T,
+      Data<T, T>
+    > = (dataSource) =>
+      dataSource.pipe(
+        map(({ data: value, resourceId }) => ({
+          context: {
+            origin: value,
+            resourceId,
+            trace: [],
+          },
+          value,
+        })),
+      );
+    this.dataSourceTransformationPipelines.push(identityTransformation);
+    return new AddSinksStepImpl(
+      this,
+      this.dataSourceTransformationPipelines,
+      this.monitorBuilderState.dialectNotificationSink,
+      this.monitorBuilderState.emailNotificationSink,
+    );
   }
 
   dispatch(strategy: DispatchStrategy): BuildStep<T> {
